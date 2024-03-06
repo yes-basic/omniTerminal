ac9329e54575e0177b456e93f3b5ebc3a023fd4a
yes-basic <135731231+yes-basic@users.noreply.github.com>
2024-03-06 10:48:27 -0500

    added library and made non functional sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
+#include <IRremote.hpp>
+#include "SPI.h"
+#include <TFT_eSPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+#define IR_RECEIVE_PIN          15  // D15
+#define IR_SEND_PIN              21  // D4
+#define TONE_PIN                27  // D27 25 & 26 are DAC0 and 1
+#define APPLICATION_PIN         16  // RX2 pin
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Serial.begin(115200);
+IrSender.begin(DISABLE_LED_FEEDBACK);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+IrSender.sendNEC(0x1, 0x40, 0);
+delay(500);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
