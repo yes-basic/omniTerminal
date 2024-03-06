26ce5552c3826365434829d07f622310d07b7f06
yes-basic <135731231+yes-basic@users.noreply.github.com>
2024-03-06 10:55:44 -0500

    made a untested receiver

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,40 @@
 #define IR_SEND_PIN              21  // D4
 #define TONE_PIN                27  // D27 25 & 26 are DAC0 and 1
 #define APPLICATION_PIN         16  // RX2 pin
-void setup() {
-Serial.begin(115200);
-IrSender.begin(DISABLE_LED_FEEDBACK);
+#include <IRremote.h>
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
 }
 
-void loop() {
-IrSender.sendNEC(0x1, 0x40, 0);
-delay(500);
+void loop(){
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        switch (results.decode_type){
+            case NEC: Serial.println("NEC"); break ;
+            case SONY: Serial.println("SONY"); break ;
+            case RC5: Serial.println("RC5"); break ;
+            case RC6: Serial.println("RC6"); break ;
+            //case DISH: Serial.println("DISH"); break ;
+            case SHARP: Serial.println("SHARP"); break ;
+            case JVC: Serial.println("JVC"); break ;
+            //case SANYO: Serial.println("SANYO"); break ;
+            //case MITSUBISHI: Serial.println("MITSUBISHI"); break ;
+            case SAMSUNG: Serial.println("SAMSUNG"); break ;
+            case LG: Serial.println("LG"); break ;
+            case WHYNTER: Serial.println("WHYNTER"); break ;
+            //case AIWA_RC_T501: Serial.println("AIWA_RC_T501"); break ;
+            case PANASONIC: Serial.println("PANASONIC"); break ;
+            case DENON: Serial.println("DENON"); break ;
+          default:
+            case UNKNOWN: Serial.println("UNKNOWN"); break ;
+          }
+        irrecv.resume();
+ }
 }
-
