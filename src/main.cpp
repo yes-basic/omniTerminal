7d47acaf88123223b92ebb5d80aafcf02a421f45
yes-basic <135731231+yes-basic@users.noreply.github.com>
2024-03-06 11:57:47 -0500

    changed order of defines and includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <IRremote.hpp>
+
 #include "SPI.h"
 #include <TFT_eSPI.h>
 
@@ -7,6 +7,8 @@
 #define IR_SEND_PIN              21  // D4
 #define TONE_PIN                27  // D27 25 & 26 are DAC0 and 1
 #define APPLICATION_PIN         16  // RX2 pin
+
+#include <IRremote.hpp>
 void setup() {
 Serial.begin(115200);
 IrSender.begin(DISABLE_LED_FEEDBACK);
