bfe358fe54ffa9623a895429c9cd63027505e0ea
yes-basic <135731231+yes-basic@users.noreply.github.com>
2024-03-08 15:44:53 -0500

    ble keyboard failed

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <BleKeyboard.h>
+#include "TFT_eSPI.h"
+BleKeyboard bleKeyboard;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if(bleKeyboard.isConnected()) {
+    Serial.println("Sending 'Hello world'...");
+    bleKeyboard.print("Hello world");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    delay(1000);
+
+    Serial.println("Sending Enter key...");
+    bleKeyboard.write(KEY_RETURN);
+
+    delay(1000);
+
+    Serial.println("Sending Play/Pause media key...");
+    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+
+    delay(1000);
+
+   //
+   // Below is an example of pressing multiple keyboard modifiers 
+   // which by default is commented out.
+    /*
+    Serial.println("Sending Ctrl+Alt+Delete...");
+    bleKeyboard.press(KEY_LEFT_CTRL);
+    bleKeyboard.press(KEY_LEFT_ALT);
+    bleKeyboard.press(KEY_DELETE);
+    delay(100);
+    bleKeyboard.releaseAll();
+    */
+  }
+
+  Serial.println("Waiting 5 seconds...");
+  delay(5000);
 }
